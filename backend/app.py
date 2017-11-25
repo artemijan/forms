1167249a05f656e5dfffce0f3b28917aa9b16d07
artem <lineartem@gmail.com>
2017-11-25 13:43:44 +0200

    disable caching

--- backend/app.py
+++ backend/app.py
@@ -18,6 +18,16 @@ def create_app(config):
         if db is not None:
             db.close()
 
+    @app.after_request
+    def add_header(response):
+        """
+        Add headers to both force latest IE rendering engine or Chrome Frame,
+        and also to cache the rendered page for 10 minutes.
+        """
+        response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+        response.headers['Cache-Control'] = 'public, max-age=0'
+        return response
+
     CORS(app, resources={r"/api/*": {"origins": "*"}})
     return app
 
